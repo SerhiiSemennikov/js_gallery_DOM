c49657ef152d8fab31b2f17e81ac0345ffc0a553
SerhiiSemennikov <semennik@hotmail.com>
2024-10-10 14:47:05 +0200

    make build

---
+++ index.9bff9adf.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#thumbs"),t=document.querySelector("#largeImg");e.onclick=function(){event.preventDefault();var e=event.target.closest(".list-item__link");e&&(t.src=e.href)};
+//# sourceMappingURL=index.9bff9adf.js.map
